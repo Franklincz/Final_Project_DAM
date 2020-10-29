87dc36b4334fc0d7737d875d051f0a2e103c425a
franklincz <cunyazetafranklin@gmail.com>
2020-10-28 19:17:14 -0500

    button y segunda activity

--- app/src/main/java/com/example/final_project_dam/secondActivity.java
+++ app/src/main/java/com/example/final_project_dam/secondActivity.java
@@ -2,25 +2,20 @@ package com.example.final_project_dam;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class secondActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_second);
     }
 
-    public void inSecActivity(View view){
-
-        startActivity(new Intent(getApplicationContext() ,secondActivity.class) );
 
 
 
-    }
 
 
 }
